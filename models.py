f6befbac6fdb8087258c1710dbe436a60fe9b8be
sathagesey33 <abdukdirmuhanad@gmail.com>
2024-02-11 16:10:25 +0300

    flask appliction

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-# models.py
+# Description: This file contains the database models for the application.
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -8,24 +8,24 @@ class User(db.Model):
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    preferences = db.Column(db.String(255))
+   # preferences = db.Column(db.String(255))
 
     # One-to-Many relationship with Mother Record Table
-    mother_records = db.relationship('MotherRecord', backref='user', lazy=True)
+   # mother_record = db.relationship('MotherRecord', backref='user', lazy=True)
 
     # One-to-Many relationship with Child Info Table
-    child_info = db.relationship('ChildInfo', backref='user', lazy=True)
-
-class MotherRecord(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    notes = db.Column(db.String(255))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    #child_info = db.relationship('ChildInfo', backref='user', lazy=True)
+   #lass MotherRecord(db.Model):
+    
+   # id = db.Column(db.Integer, primary_key=True)
+    #notes = db.Column(db.String(255))
+    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     # Many-to-One relationship with User Table
-    user = db.relationship('User', backref=db.backref('mother_records', lazy=True))
+    #user = db.relationship('User', backref=db.backref('mother_record', lazy=True))
 
     # One-to-Many relationship with Child Info Table
-    child_info = db.relationship('ChildInfo', backref='mother_record', lazy=True)
+    #child_info = db.relationship('ChildInfo', backref='mother_record', lazy=True)
 
 class ChildInfo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,30 +38,30 @@ class ChildInfo(db.Model):
    # mother_id = db.Column(db.Integer, db.ForeignKey('mother_record.id'), nullable=False)
 
     # Many-to-One relationship with User Table
-    user = db.relationship('User', backref=db.backref('child_info', lazy=True))
+   # user = db.relationship('User', backref=db.backref('child_info', lazy=True))
 
     # Many-to-One relationship with Mother Record Table
-    mother_record = db.relationship('MotherRecord', backref=db.backref('child_info', lazy=True))
+    #mother_record = db.relationship('MotherRecord', backref=db.backref('child_info', lazy=True))
 
     # One-to-Many relationship with Journal Entries Table
-    journal_entries = db.relationship('JournalEntry', backref='child_info', lazy=True)
+    #journal_entries = db.relationship('JournalEntry', backref='child_info', lazy=True)
 
-class JournalEntry(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    entry_date = db.Column(db.Date)
-    entry_text = db.Column(db.Text)
-    child_id = db.Column(db.Integer, db.ForeignKey('child_info.id'), nullable=False)
-
-    # Many-to-One relationship with Child Info Table
-    child_info = db.relationship('ChildInfo', backref=db.backref('journal_entries', lazy=True))
-
-class VaccinationSchedule(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    vaccine_name = db.Column(db.String(50), nullable=False)
-    recommended_age = db.Column(db.String(20))
-    reminder_days_before = db.Column(db.Integer)
-    child_info_id = db.Column(db.Integer, db.ForeignKey('child_info.id'), nullable=False)
+#class JournalEntry(db.Model):
+   # id = db.Column(db.Integer, primary_key=True)
+    #entry_date = db.Column(db.Date)
+    #entry_text = db.Column(db.Text)
+    #child_id = db.Column(db.Integer, db.ForeignKey('child_info.id'), nullable=False)
 
     # Many-to-One relationship with Child Info Table
-    child_info = db.relationship('ChildInfo', backref=db.backref('vaccination_schedule', lazy=True))
+    #child_info = db.relationship('ChildInfo', backref=db.backref('journal_entries', lazy=True))
+
+#class VaccinationSchedule(db.Model):
+   # id = db.Column(db.Integer, primary_key=True)
+    #vaccine_name = db.Column(db.String(50), nullable=False)
+    #recommended_age = db.Column(db.String(20))
+    #reminder_days_before = db.Column(db.Integer)
+    #child_info_id = db.Column(db.Integer, db.ForeignKey('child_info.id'), nullable=False)
+
+    # Many-to-One relationship with Child Info Table
+   # child_info = db.relationship('ChildInfo', backref=db.backref('vaccination_schedule', lazy=True))
 

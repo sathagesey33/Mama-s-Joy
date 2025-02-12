f7c07bf9611b3af36ac29fe327c6fcf122f93e4b
254guru <otienooluda@gmail.com>
2025-02-12 15:25:03 +0300

    initialize backend structure

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,17 +1,21 @@
 from flask import Flask
+from flask_cors import CORS
+from config import config
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from flask_bcrypt import Bcrypt
 
-app = Flask(__name__)
-bcrypt = Bcrypt(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Use SQLite for simplicity
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://@localhost/baby_growth'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key'
-
 db = SQLAlchemy()
-db.init_app(app) 
+migrate = Migrate()
 
-from app import routes
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    CORS(app)
+    bcrypt = Bcrypt(app)
+    migrate.init_app(app, db)
+
+    from app import routes 
